0c917561eb0b6035d0f33d7bc31b9683877ceeba
Osher El Netanany <osher.filter@gmail.com>
2022-01-30 11:46:52 +0200

    starwars - savepoint, dal works

---
+++ starwars/lib/starwars-dal/index.js
@@ -0,0 +1,42 @@
+const request = require('request');
+/*
+javascript hoisting
+
+let const => block ScriptProcessorNode
+function, var => function scope, hoisted
+*/
+
+module.exports = ({baseUrl}) => {
+
+	return {
+		people: {
+			byId: byIdFetcher('people'),
+		},
+    vehicle: {
+			byId: byIdFetcher('vehicles'),
+		},
+		planet: {
+			list: () => 1,
+			byId: byIdFetcher('planets'),
+		}
+	};
+
+  function byIdFetcher(entity) {
+    return (id) => getJson(`${baseUrl}/${entity}/${id}`)
+  }
+
+  function getJson(url) {
+    return new Promise((accept, reject) => {
+      request(
+        url,
+        { json: true },
+        (err, {
+          body,
+        }) => {
+          if (err) return reject(err);
+          accept(body);
+        }
+      );
+    })
+  };
+}
