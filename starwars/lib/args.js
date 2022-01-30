764eb35607444d05a29adf246273a5decae6a994
Osher El Netanany <osher.filter@gmail.com>
2022-01-30 16:25:04 +0200

    add logger module(need to use it)

--- starwars/lib/args.js
+++ starwars/lib/args.js
@@ -9,9 +9,10 @@ module.exports = ({
 }) => {
   const args = argv.slice(2);
   const switches = minimist(args, {});
+  const { name, version } = require('../package');
 
   const error = 
     switches.id && 'number' != typeof switches.id && '--id must parse into a number';
 
-  return merge(config, switches, { error });
+  return merge(config, switches, { package: { name, version }, error });
 };
---
+++ starwars/lib/logger.js
@@ -0,0 +1,45 @@
+const { assign } = Object;
+const pino = require('pino');
+module.exports = ({
+  logger: {
+    levels,
+    prettyPrint,
+    prettyOptions,
+  },
+  package: { name, version },
+}) => {
+
+    const options = {
+      channel: 'swappi-cli',
+      level: levels.default,
+    };
+
+    if (  fs.existsSync('./node_modules/pino-pretty')
+      && prettyPrint
+      && prettyOptions
+    ) {
+      options.transport = {
+        target: 'pino-pretty',
+        options: prettyOptions,
+      }
+    }
+
+    const baseLogger = pino(options).child({
+      pkg: { name, version }
+    });
+    
+    const of = channel => {
+      const childLogger = baseLogger.child({ channel });
+
+      const level = levels[channel] || levels.default;
+      childLogger.level = level;
+      childLogger.of = of;
+      return childLogger;
+    };
+
+    baseLogger.of = of;
+
+    return baseLogger;
+}
+
+
